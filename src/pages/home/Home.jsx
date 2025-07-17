d684ed3903deb8b3cc93cdfc89b375c2ea6169cb
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-07-17 14:31:36 +0500

    lesson006

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Header from "../../components/header/header";
+
 import PostCard from "../../components/postCard/PostCard";
 import Hero from "../../components/hero/Hero";
 
 const Home = () => {
-    return <div>
-     
-        <Hero/>
-        <PostCard/>
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <PostCard />
+    </div>
+  );
 };
 
 export default Home;
